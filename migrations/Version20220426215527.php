9f63ae2e14a57d8b269a4eca6225708df2f32bfa
Michael Owen Jones <michael.jowen.jones@gmail.com>
2022-04-26 23:07:29 +0100

    feature(migrations): added the new entities migrations

--- migrations/Version20220426215527.php
+++ migrations/Version20220426215527.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220311182202 extends AbstractMigration
+final class Version20220426215527 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -19,36 +19,29 @@ final class Version20220311182202 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('CREATE SEQUENCE dogs_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE TABLE dogs (id INT NOT NULL, username VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE UNIQUE INDEX uniq_812c397df85e0677 ON dogs (username)');
-
+        // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SEQUENCE comment_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE SEQUENCE post_emoji_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE comment (id INT NOT NULL, post_id INT NOT NULL, dog_id INT NOT NULL, comment_text TEXT NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_9474526CE85F12B8 ON comment (post_id)');
         $this->addSql('CREATE INDEX IDX_9474526CE4E2D712 ON comment (dog_id)');
         $this->addSql('COMMENT ON COLUMN comment.created_at IS \'(DC2Type:datetime_immutable)\'');
-
         $this->addSql('CREATE TABLE post_emoji (id INT NOT NULL, dog_id INT NOT NULL, value VARCHAR(7) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_21507ADDE4E2D712 ON post_emoji (dog_id)');
         $this->addSql('COMMENT ON COLUMN post_emoji.created_at IS \'(DC2Type:datetime_immutable)\'');
-
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CE85F12B8 FOREIGN KEY (post_id) REFERENCES post (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CE4E2D712 FOREIGN KEY (dog_id) REFERENCES dogs (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
+        $this->addSql('ALTER TABLE post_emoji ADD CONSTRAINT FK_21507ADDE4E2D712 FOREIGN KEY (dog_id) REFERENCES dogs (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
         $this->addSql('ALTER TABLE post ADD post_text TEXT NOT NULL');
         $this->addSql('ALTER TABLE post DROP text');
         $this->addSql('ALTER TABLE post DROP emojis_id');
         $this->addSql('ALTER TABLE post DROP comments_id');
-
-        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CE85F12B8 FOREIGN KEY (post_id) REFERENCES post (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE comment ADD CONSTRAINT FK_9474526CE4E2D712 FOREIGN KEY (dog_id) REFERENCES dogs (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
-        $this->addSql('ALTER TABLE post_emoji ADD CONSTRAINT FK_21507ADDE4E2D712 FOREIGN KEY (dog_id) REFERENCES dogs (id) NOT DEFERRABLE INITIALLY IMMEDIATE');
     }
 
     public function down(Schema $schema): void
     {
+        // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE dogs_id_seq CASCADE');
-        $this->addSql('DROP TABLE dogs');
         $this->addSql('DROP SEQUENCE comment_id_seq CASCADE');
         $this->addSql('DROP SEQUENCE post_emoji_id_seq CASCADE');
         $this->addSql('DROP TABLE comment');
