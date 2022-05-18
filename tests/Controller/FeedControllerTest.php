e59d43c71765eea52d09aba57ab798daa3b19538
Edward Heaver <edward.heaver01@gmail.com>
2022-05-18 21:28:43 +0100

    Tidy up test

--- tests/Controller/FeedControllerTest.php
+++ tests/Controller/FeedControllerTest.php
@@ -12,33 +12,20 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class FeedControllerTest extends WebTestCase
 {
-    private DogRepository $dogRepo;
-    private PostRepository $postRepo;
-
-    public function setUp(): void
-    {
-        parent::setUp();
-        static::bootKernel();
-
-        $this->dogRepo = static::getContainer()->get(DogRepository::class);
-        $this->postRepo = static::getContainer()->get(PostRepository::class);
-    }
-
     public function testItSubmitAndCreatePost(): void
     {
         $client = static::createClient();
-
-        $testUser = $this->dogRepo->findOneByUsername('testUser');
+        $dogRepo = static::getContainer()->get(DogRepository::class);
+        $postRepo = static::getContainer()->get(PostRepository::class);
+        $testUser = $dogRepo->findOneByUsername('testUser');
         $client->loginUser($testUser);
 
         $crawler = $client->request('GET','/add-post');
-        
         $form = $crawler->selectButton('Save')->form();
         $form["post_form[post_text]"] = "testing post";
-        
         $crawler = $client->submit($form);
         
-        $newPost = $this->postRepo->findBy(['post_text' => 'testing post'])[0];
+        $newPost = $postRepo->findBy(['post_text' => 'testing post'])[0];
         $this->assertTrue($newPost->getPostText() === 'testing post');
         $client->followRedirects(true);
     }
