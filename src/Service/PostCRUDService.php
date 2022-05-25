a760ef228ed8465a31fefa4fa9a0b5ca627100cd
Michael Owen Jones <michael.jowen.jones@gmail.com>
2022-05-25 23:03:48 +0100

    refactor(service): added comment add function

--- src/Service/PostCRUDService.php
+++ src/Service/PostCRUDService.php
@@ -2,24 +2,29 @@
 
 namespace App\Service;
 
+use App\Entity\Dog;
+use App\Entity\Comment;
 use App\Repository\PostRepository;
+use App\Repository\CommentRepository;
 use App\Entity\Post;
 
 class PostCRUDService 
 {
     private PostRepository $postRepository;
+    private CommentRepository $commentRepository;
 
     public function __construct(PostRepository $postRepository)
     {
         $this->postRepository = $postRepository;
     }
 
-    /**
-     * @accepts Dog $dog
-     * @returns void
-     */
     public function savePost(Post $post): void
     {   
         $this->postRepository->add($post);
+    }
+
+    public function saveCommentForPost(Dog $dogUser, Comment $comment): void 
+    {
+        // $this->commentRepository->setPost();
     }
 }
