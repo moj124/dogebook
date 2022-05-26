503891ca8f90817c53fb63cdfca0371b8ff1eee2
Michael Owen Jones <michael.jowen.jones@gmail.com>
2022-05-26 21:25:18 +0100

    refactor(service): remove comment handler post

--- src/Service/PostCRUDService.php
+++ src/Service/PostCRUDService.php
@@ -2,29 +2,20 @@
 
 namespace App\Service;
 
-use App\Entity\Dog;
-use App\Entity\Comment;
 use App\Repository\PostRepository;
-use App\Repository\CommentRepository;
 use App\Entity\Post;
 
 class PostCRUDService 
 {
     private PostRepository $postRepository;
-    private CommentRepository $commentRepository;
 
     public function __construct(PostRepository $postRepository)
-    {
+    { 
         $this->postRepository = $postRepository;
     }
 
     public function savePost(Post $post): void
     {   
         $this->postRepository->add($post);
-    }
-
-    public function saveCommentForPost(Dog $dogUser, Comment $comment): void 
-    {
-        // $this->commentRepository->setPost();
     }
 }
