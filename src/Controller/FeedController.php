6893d2f15b5396fe0f0bf382fa8fb0bc65c33978
Michael Owen Jones <michael.jowen.jones@gmail.com>
2022-05-28 23:48:49 +0100

    refactor(controller): fix code structure and move to service add comment

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -11,19 +11,19 @@ use App\Repository\PostRepository;
 use App\Repository\CommentRepository;
 use App\Form\PostFormType;
 use App\Form\CommentFormType;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class FeedController extends AbstractController
 {
-    public function index(DogCRUDService $dogService, PostRepository $postRepository, CommentRepository $commentRepository): Response
+    public function index(DogCRUDService $dogService, PostRepository $postRepository, CommentRepository $commentRepository): Response 
     {
+
         $dogUser = $this->getUser();
-        $dogUsername = $dogUser->getUserIdentifier();
-        // dd($dogUsername);
+
         $postAuthor = $dogService->getDogNiceName($dogUser);
+
         $posts = $postRepository->findAllPostsByDog($dogUser);
 
         if(count($posts) === 0) {
@@ -34,35 +34,38 @@ class FeedController extends AbstractController
 
         $comments = $commentRepository->findAll();
 
-        return $this->render('feed/feed.html.twig', 
-        [
-            'posts' => $posts,
-            'currentDogUser' => $postAuthor,
-            'comments' => $comments,
+        return $this->render(
+            'feed/feed.html.twig', 
+            [
+                'posts' => $posts,
+                'currentDogUser' => $postAuthor,
+                'comments' => $comments,
             ]
         );
     }
 
-    public function createComment(Request $request, EntityManagerInterface $em , Post $post) : Response
+    public function createComment(Request $request, PostCRUDService $postService ,Post $post) : Response
     {
         $comment = new Comment();
-        $comment->setPost($post);
-        $form = $this->createForm(CommentFormType::class, $comment);
 
+        $form = $this->createForm(CommentFormType::class, $comment);
+        
         $dogUser = $this->getUser();
-        $comment->setDog($dogUser);
+
         $form->handleRequest($request);
         
         if($form->isSubmitted() && $form->isValid()) {
-            $em->persist($comment);
-            $em->flush();
+            $postService->addPostComment($comment, $dogUser, $post);
             return $this->redirectToRoute('feed');
         }
 
         // Rendering the view if the form has not been submitted
-        return $this->render('feed/comment/add-comment.html.twig', [
-            'form' => $form->createView(),
-        ]);
+        return $this->render(
+            'feed/comment/add-comment.html.twig', 
+            [
+                'form' => $form->createView(),
+            ]
+        );
     }
 
     public function createPost(Request $request, DogCRUDService $dogService): Response 
@@ -80,8 +83,11 @@ class FeedController extends AbstractController
         }
 
         // Rendering the view if the form has not been submitted
-        return $this->render('feed/post/add-post.html.twig', [
-            'form' => $form->createView(),
-        ]);
+        return $this->render(
+            'feed/post/add-post.html.twig',
+            [
+                'form' => $form->createView(),
+            ]
+        );
     }
 }
