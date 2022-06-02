c7caa5066b5425409e7eb599b5fb6529a7da99f7
Michael Owen Jones <michael.jowen.jones@gmail.com>
2022-06-03 00:12:27 +0100

    feature(test): added add comment test to feed control

--- tests/Controller/FeedControllerTest.php
+++ tests/Controller/FeedControllerTest.php
@@ -6,6 +6,7 @@ use App\DataFixtures\PostFixture;
 use App\Entity\Post;
 use App\Entity\Dog;
 use App\Repository\DogRepository;
+use App\Repository\CommentRepository;
 use App\Repository\PostRepository;
 use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
@@ -14,12 +15,14 @@ class FeedControllerTest extends WebTestCase
 {
     private $client;
     private $dogRepo;
+    private $commentRepo;
     private $postRepo;
 
     public function setUp(): void
     {
         $this->client = static::createClient();
         $this->dogRepo = static::getContainer()->get(DogRepository::class);
+        $this->commentRepo = static::getContainer()->get(CommentRepository::class);
         $this->postRepo = static::getContainer()->get(PostRepository::class);
 
     }
@@ -38,18 +41,59 @@ class FeedControllerTest extends WebTestCase
         $this->assertTrue($newPost->getPostText() === 'testing post');
     }
 
-    public function testItCanEditAnExistingPost(): void
+    public function testSubmitAndCreateComment(): void
     {
         $testUser = $this->dogRepo->findOneByUsername('testUser');
         $this->client->loginUser($testUser);
-
-        // We will need to find a new form here to do the edit
-        // Posts exist for the testUser already and their content is set. Just need to update it
-        // Should assert that the db was updated
+        $testUserPost = $this->postRepo->findBy(['post_text' => 'Woof woofingtons'])[0];
+        $url = "/post/{$testUserPost->getId()}/add-comment";
+        $crawler = $this->client->request('GET', $url);
+        $form = $crawler->selectButton('Save')->form();
+        $form["comment_form[comment_text]"] = 'testing comment';
+        $crawler = $this->client->submit($form);
+        
+        $newComment = $this->commentRepo->findBy(['comment_text' => 'testing comment'])[0];
+        $this->assertTrue($newComment->getCommentText() === 'testing comment');
     }
 
-    public function testItCanDeleteAPost(): void
-    {
-        // Should be able to log in as a user and delete a post by id
-    }
+    // public function testAddDogToPack(): void
+    // {
+
+    // }
+
+    // public function testViewNonPackDogs(): void
+    // {
+
+    // }
+
+    // public function testItCanEditAnExistingPost(): void
+    // {
+    //     $testUser = $this->dogRepo->findOneByUsername('testUser');
+    //     $this->client->loginUser($testUser);
+
+    //     // We will need to find a new form here to do the edit
+    //     // Posts exist for the testUser already and their content is set. Just need to update it
+    //     // Should assert that the db was updated
+    // }
+
+    // public function testItCanDeleteAPost(): void
+    // {
+    //     // Should be able to log in as a user and delete a post by id
+    // }
+
+    // public function testItCanDeleteComment(): void
+    // {
+    //     // Should be able to log in as a user and delete a post by id
+    // }
+
+    // public function testItCanRemovePackMember(): void
+    // {
+    //     // Should be able to log in as a user and delete a post by id
+    // }
+
+    // public function testItCanRemoveSelfFromPack(): void
+    // {
+    //     // Should be able to log in as a user and delete a post by id
+    // }
+
 }
