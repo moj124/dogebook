3905cc861478087b100d077553de70fd549bbe94
Michael Owen Jones <michael.jowen.jones@gmail.com>
2022-06-11 22:36:18 +0100

    refactor(service): move pack functionality to packService

--- src/Service/PackService.php
+++ src/Service/PackService.php
@@ -6,6 +6,7 @@ use App\Entity\Dog;
 use App\Domain\Facade\Cache;
 use App\Message\ActivityEvents\FriendAddedEvent;
 use App\Repository\DogRepository;
+use Lagdo\Symfony\Facades\Log;
 use Psr\Cache\CacheItemInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
 
@@ -47,5 +48,45 @@ class PackService
         Cache::delete(self::PACK_IDENTIFIER . $beta->getId());
 
         $this->bus->dispatch(new FriendAddedEvent($alpha, $beta));
+
+        Log::info(`PackService@addDogToPack has added alpha Dog & beta Dog to each other's packs`, ['alpha' => $alpha, 'beta' => $beta],);
+    }
+
+    /**
+     * @paramter Dog[] $dogs
+     * @paramter Dog[] $myPack
+     * @return Dog[]
+     */
+    public function getDogsNotInPack(Dog $dogUser,array $myPack, array $dogs): array
+    {
+        if(count($dogs) === 0) {
+            return $dogs;
+        }
+
+        $myPack = array_map(fn(Dog $dog): int => $dog->getId(), $myPack);
+
+        $otherDogUsers = [];
+
+        foreach ( $dogs as $dog) {
+            if (!in_array($dog->getId(),$myPack) && $dog->getId() !== $dogUser->getId()) {
+                array_push($otherDogUsers,$dog);
+            }
+        }
+
+        return $otherDogUsers;
+    }
+
+    public function removeDogFromPack(Dog $dogUser, Dog $dog): void
+    {
+        $dogUser->removeDogFromPack($dog);
+        $dog->removeDogFromPack($dogUser);
+
+        $this->dogRepo->add($dogUser);
+        $this->dogRepo->add($dog);
+
+        Cache::delete(self::PACK_IDENTIFIER . $dogUser->getId());
+        Cache::delete(self::PACK_IDENTIFIER . $dog->getId());
+
+        Log::info(`PackService@removeDogToPack has been removed from Dog's Packs`, ['dog' => $dog,],);
     }
 }
